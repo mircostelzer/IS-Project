774d582121fe6730b995da5e08a7c40b51c00e87
filippoxausa <filippoxausa@gmail.com>
2025-02-10 19:15:21 +0100

    Added changePassword method

--- backend/routes/usersRoutes.js
+++ backend/routes/usersRoutes.js
@@ -1,11 +1,12 @@
 import { Router } from 'express';
 const router = Router();
-import { getUsers, getUserById, createUser, deleteUser } from '../controllers/usersController.js';
+import { getUsers, getUserById, createUser, updatePassword, deleteUser } from '../controllers/usersController.js';
 import { tokenChecker, verifyRole } from '../authentication/verification.js';
 
 router.get('/', tokenChecker, verifyRole('operator'), getUsers);
 router.get('/:id', tokenChecker, verifyRole('operator'), getUserById);
 router.post('/', createUser);
+router.put('/:id/password', tokenChecker, updatePassword);
 router.delete('/:id', tokenChecker, verifyRole('operator'), deleteUser);
 
 export default router;
