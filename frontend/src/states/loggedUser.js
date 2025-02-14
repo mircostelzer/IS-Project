683998e7cae4f31a247636bf6f7d774c4498ecd2
Amar <amar.sidkir@studenti.unitn.it>
2025-02-14 14:16:36 +0100

    Extended cookie duration

--- frontend/src/states/loggedUser.js
+++ frontend/src/states/loggedUser.js
@@ -35,7 +35,7 @@ function setLoggedUser(data) {
     loggedUser.updatedAt = data.updatedAt;
 
     // Save to cookies
-    Cookies.set('loggedUser', JSON.stringify(loggedUser), { expires: 30 }); // expires in 1 month
+    Cookies.set('loggedUser', JSON.stringify(loggedUser), { expires: 90 }); // expires in 3 months
 }
 
 function clearLoggedUser() {
